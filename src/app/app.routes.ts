80ed348cedb496145e21788672b4529f8df1d64a
mariammostafam888 <mariammostafam888@gmail.com>
2026-06-09 02:27:05 +0300

    update project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
-import { HomeComponent } from './pages/home/home.component';
 import { Signup } from './pages/signup/signup';
 import { Login } from './pages/login/login';
 
 export const routes: Routes = [
-    {path:'trip-form', loadComponent: () => import('./pages/trip-form/trip-form').then(m => m.TripForm)},
+  { path: '', component: LandingComponent },
+  { path: 'signup', component: Signup },
+  { path: 'login', component: Login},
+  { path: '**', redirectTo: '' }
 ];
